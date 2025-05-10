9d31a94d19b5fbc0133413b4cd3754bfc24feb4b
Your Name <you@example.com>
2025-05-10 17:38:35 +0200

    ajout page portfolio

--- src/types/models.ts
+++ src/types/models.ts
@@ -2,7 +2,7 @@ export interface Project {
     Image: string[];
     Title?: string[];
     Description: string[];
-    Link?: string[];
+    Link?: string;
     ButtonHref?: string;
     TitleButton?: string;
 }
